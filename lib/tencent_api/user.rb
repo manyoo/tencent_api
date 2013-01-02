6e361958031b3e6db03ff40a0721bfa490ef4757
Eric Wong <ericsyw@gmail.com>
2013-01-02 23:43:16 +0800

    fix bugs

--- lib/tencent_api/user.rb
+++ lib/tencent_api/user.rb
@@ -2,35 +2,35 @@ module TencentApi
   module User
     class << self
       def get_info options, &blocks
-        get_remote '/v3/user/get_info', options, &blocks
+        TencentApi::get_remote '/v3/user/get_info', options, &blocks
       end
 
       def get_multi_info options, &blocks
-        get_remote '/v3/user/get_multi_info', options, &blocks
+        TencentApi::get_remote '/v3/user/get_multi_info', options, &blocks
       end
 
       def total_vip_info options, &blocks
-        get_remote '/v3/user/total_vip_info', options, &blocks
+        TencentApi::get_remote '/v3/user/total_vip_info', options, &blocks
       end
 
       def is_vip options, &blocks
-        get_remote '/v3/user/is_vip', options, &blocks
+        TencentApi::get_remote '/v3/user/is_vip', options, &blocks
       end
 
       def friends_vip_info options, &blocks
-        get_remote '/v3/user/friends_vip_info', options, &blocks
+        TencentApi::get_remote '/v3/user/friends_vip_info', options, &blocks
       end
 
       def is_setup options, &blocks
-        get_remote '/v3/user/is_setup', options, &blocks
+        TencentApi::get_remote '/v3/user/is_setup', options, &blocks
       end
 
       def is_login options, &blocks
-        get_remote '/v3/user/is_login', options, &blocks
+        TencentApi::get_remote '/v3/user/is_login', options, &blocks
       end
 
       def is_area_login options, &blocks
-        get_remote '/v3/user/is_area_login', options, &blocks
+        TencentApi::get_remote '/v3/user/is_area_login', options, &blocks
       end
     end
   end
